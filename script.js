0f8cfea08f676cbb4afd9d5ebbf34ccc42c3ac06
ishwardhanuka <ishwardhanuka@gmail.com>
2016-04-21 21:19:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Glad to see you! What brings you here? iOS or Data?')
                 .then(() => 'speak');
         }
     },
